77a1cd0d4ac713bdf1651908522eda566897dd4d
ngogiaphat <ngogiaphat0802@gmail.com>
2022-11-28 00:53:50 +0700

    the first

---
+++ src/customs/sapphire/CatClient.ts
@@ -0,0 +1,17 @@
+import { emoji, trans } from '@cat-services/i18n';
+import { container, SapphireClient } from '@sapphire/framework';
+declare module '@sapphire/pieces' 
+{
+	interface Container 
+	{
+		trans: Function;
+		emoji: Function;
+	}
+}
+export class CatClient extends SapphireClient 
+{
+	public i18n = () => {
+		container.trans = trans;
+		container.emoji = emoji;
+	};
+}
